fa5f319fc289ece28a448df06e682f3a76b1f113
vitória lopes <heyvitoria.lopes@gmail.com>
2023-06-09 01:37:27 -0300

    new: tests

---
+++ src/main/java/tests/TestsClient.java
@@ -0,0 +1,71 @@
+package tests;
+
+import io.grpc.*;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+public class TestsClient {
+    private static final Logger logger = Logger.getLogger(TestsClient.class.getName());
+
+    private final tests.GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    public TestsClient(Channel channel) {
+        blockingStub = GreeterGrpc.newBlockingStub(channel);
+    }
+
+    public void emptyArgsEmptyReturn() {
+        Empty empty = new Empty();
+        Empty response = blockingStub.emptyArgsEmptyReturn(empty);
+        logger.info("stringArgsStringReturn: " + response);
+    }
+
+    public void longArgsLongReturn() {
+        Long request = Long.newBuilder().setNumber(1).build();
+        Long response = blockingStub.longArgsLongReturn(request);
+        logger.info("longArgsLongReturn: " + response);
+    }
+
+    public void multiLongArgsLongReturn() {
+        /*
+         * @TODO: fazer funcionar :)
+        MultiLong request = MultiLong.newBuilder()
+                .setNumber1(1)
+                .setNumber2(2)
+                .setNumber3(3)
+                .setNumber4(4)
+                .setNumber5(5)
+                .setNumber6(6)
+                .setNumber7(7)
+                .setNumber8(8)
+                .build();
+        Long response = blockingStub.multiLongArgsLongReturn(request);
+        logger.info("longArgsLongReturn: " + response);
+         */
+    }
+
+    public void stringArgsStringReturn() {
+        /*
+        * @TODO: implementar diferentes tamanhos
+        * */
+        StringMessage request = StringMessage.newBuilder().setMessage("oi").build();
+        StringMessage response = blockingStub.stringArgsStringReturn(request);
+        logger.info("stringArgsStringReturn: " + response);
+    }
+
+    public static void main(java.lang.String[] args) throws Exception {
+        java.lang.String target = "localhost:50051";
+
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
+
+        try {
+            TestsClient client = new TestsClient(channel);
+            client.emptyArgsEmptyReturn();
+            client.longArgsLongReturn();
+            client.multiLongArgsLongReturn();
+            client.stringArgsStringReturn();
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+    }
+}
--- src/main/java/tests/TestsServer.java
+++ src/main/java/tests/TestsServer.java
@@ -1,24 +1,22 @@
-package helloworld;
+package tests;
 
 import io.grpc.Grpc;
 import io.grpc.InsecureServerCredentials;
 import io.grpc.stub.StreamObserver;
+
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 
-/**
- * Server that manages startup/shutdown of a {@code Greeter} server.
- */
-public class HelloWorldServer {
-    private static final Logger logger = Logger.getLogger(HelloWorldServer.class.getName());
+public class TestsServer {
+    private static final Logger logger = Logger.getLogger(TestsServer.class.getName());
 
-    private io.grpc.Server server;
+    private static io.grpc.Server server;
 
     private void start() throws IOException {
         int port = 50051;
         server = Grpc.newServerBuilderForPort(port, InsecureServerCredentials.create())
-                .addService(new GreeterImpl())
+                .addService(new TestsImpl())
                 .build()
                 .start();
         logger.info("Server started, listening on " + port);
@@ -27,7 +25,7 @@ public class HelloWorldServer {
             public void run() {
                 System.err.println("*** shutting down gRPC server since JVM is shutting down");
                 try {
-                    HelloWorldServer.this.stop();
+                    TestsServer.this.stop();
                 } catch (InterruptedException e) {
                     e.printStackTrace(System.err);
                 }
@@ -49,14 +47,34 @@ public class HelloWorldServer {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        final HelloWorldServer helloWorldServer = new HelloWorldServer();
-        helloWorldServer.start();
-        helloWorldServer.blockUntilShutdown();
+        final TestsServer testsServer = new TestsServer();
+        testsServer.start();
+        testsServer.blockUntilShutdown();
     }
 
-    static class GreeterImpl extends GreeterGrpc.GreeterImplBase {
-        public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
-            HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + req.getName()).build();
+    static class TestsImpl extends GreeterGrpc.GreeterImplBase {
+        public void emptyArgsEmptyReturn(StreamObserver<Empty> responseObserver) {
+            responseObserver.onNext(null);
+            responseObserver.onCompleted();
+        }
+
+        public void longArgsLongReturn(Long req, StreamObserver<Long> responseObserver) {
+            Long reply = Long.newBuilder().setNumber(req.getNumber()).build();
+
+            responseObserver.onNext(reply);
+            responseObserver.onCompleted();
+        }
+
+        public void multiLongArgsLongReturn(MultiLong req, StreamObserver<Long> responseObserver) {
+            MultiLong reply = MultiLong.newBuilder().build();
+
+            responseObserver.onNext(reply.getNumber1());
+            responseObserver.onCompleted();
+        }
+
+        public void stringArgsStringReturn(StringMessage req, StreamObserver<StringMessage> responseObserver) {
+            StringMessage reply = StringMessage.newBuilder().setMessage(req.getMessage()).build();
+
             responseObserver.onNext(reply);
             responseObserver.onCompleted();
         }
